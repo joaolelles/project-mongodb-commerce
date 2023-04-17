91a67350ed8c45c6c32107a5c9e33b10ff8bd589
João <joaolelles7@gmail.com>
2023-04-17 18:48:52 -0300

    Requisito 11

---
+++ challenges/desafio11.js
@@ -0,0 +1,13 @@
+db.produtos.find({
+    $nor: [{
+        nome: "Big Mac",
+    },
+    {
+        nome: "McChicken",
+    }],
+}, {
+    nome: 1,
+    vendidos: 1,
+    curtidas: 1,
+    _id: 0,
+});
