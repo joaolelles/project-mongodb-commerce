d52eb8887c568d63760cec4b012952e69f927fec
João <joaolelles7@gmail.com>
2023-04-17 17:22:47 -0300

    Requisito 5, fix

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -3,13 +3,13 @@ db.produtos.find({
     $or: [{
         curtidas: {
             $eq: 36,
-        }
+        },
     },
     {
         vendidos: {
             $eq: 85,
         },
-    }]
+    },]
 }, {
     nome: 1,
     curtidas: 1,
