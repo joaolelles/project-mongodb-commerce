4dfc08cdab9fef6c723f6c9cfd58db5cd1c171f0
João <joaolelles7@gmail.com>
2023-04-17 17:31:30 -0300

    Requisito 7

---
+++ challenges/desafio7.js
@@ -0,0 +1,14 @@
+db.produtos.find({
+    $and: [{
+        vendidos: {
+            $ne: 50,
+        },
+        tags: {
+            $exists: 0
+        },
+    }],
+}, {
+    nome: 1,
+    vendidos: 1,
+    _id: 0,
+})
