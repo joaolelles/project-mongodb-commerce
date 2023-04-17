e8d78a3a3c158ec8efa29128c0aa96a474ba8a76
João <joaolelles7@gmail.com>
2023-04-17 17:41:25 -0300

    Requisito 8

---
+++ challenges/desafio8.js
@@ -0,0 +1,15 @@
+db.produtos.deleteMany({
+    curtidas: {
+        $lt: 50,
+    },
+}, {
+    nome: 1,
+    curtidas: 1,
+    _id: 0,
+});
+
+db.produtos.find({},
+    {
+        nome: 1,
+        _id: 0,
+    });
